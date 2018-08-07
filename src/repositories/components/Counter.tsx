238cb06014428ce6516d3daf9385fe715e434a4c
Luis Hernandez <luis.hlopez@outlook.com>
2018-08-07 11:48:57 -0500

    Create Counter component

---
+++ src/repositories/components/Counter.tsx
@@ -0,0 +1,36 @@
+import React, { SFC } from 'react';
+import { RepositoryCounter } from '../../models/repository-counter.enum';
+
+import './Counter.css';
+
+interface CounterProps {
+  type: RepositoryCounter;
+  count: number;
+}
+
+const Counter: SFC<CounterProps> = ({ type, count }) => {
+  let icon: string;
+  switch (type) {
+    case RepositoryCounter.Stars:
+      icon = 'fa-star';
+      break;
+    case RepositoryCounter.Forks:
+      icon = 'fa-code-fork';
+      break;
+    case RepositoryCounter.Watchers:
+      icon = 'fa-eye';
+      break;
+    default:
+      icon = 'fa-exclamation-circle';
+      break;
+  }
+
+  return (
+    <div className="counter">
+      <i className={`counter__icon fa ${icon}`} />
+      <span className="counter__count">{count}</span>
+    </div>
+  );
+};
+
+export default Counter;
